2121ae35ed1e124cf6428fe52961b08b99f35a5d
Bryan Brzycki <bbrzycki@berkeley.edu>
2018-07-09 17:21:52 -0400

    Write new pipeline to split and search using pandas

---
+++ scripts/pipeline.py
@@ -0,0 +1,141 @@
+import numpy as np
+import pandas as pd
+from blimpy import read_header, Waterfall, Filterbank
+
+import matplotlib.pyplot as plt
+
+import sys, os, glob
+sys.path.append("../")
+import setigen as stg
+
+# import edited version of turbo_seti
+sys.path.insert(1,'/datax/users/bryanb/turbo_seti')
+from turbo_seti.findoppler.findopp import FinDoppler
+
+######
+
+f_sample_num = 2**20
+
+input_fn = '/mnt_bls0/datax3/collate/AGBT17B_999_70/spliced_blc00010203040506o7o0111213141516o7o0212223242526o7o031323334353637_guppi_58090_40453_HIP43223_0021.gpuspec.0000.fil'
+output_dir = '/datax/users/bryanb/data/'
+
+print('Begin splitting input Filterbank file')
+
+split_fns = stg.split_fil(input_fn, output_dir, f_sample_num)
+
+######
+
+# This is a hack... necessary to make turbo_seti work?
+obs_info = {}
+obs_info['pulsar'] = 0  # Bool if pulsar detection.
+obs_info['pulsar_found'] = 0  # Bool if pulsar detection.
+obs_info['pulsar_dm'] = 0.0  # Pulsar expected DM.
+obs_info['pulsar_snr'] = 0.0 # SNR
+obs_info['pulsar_stats'] = np.zeros(6)
+obs_info['RFI_level'] = 0.0
+obs_info['Mean_SEFD'] = 0.0
+obs_info['psrflux_Sens'] = 0.0
+obs_info['SEFDs_val'] = [0.0]
+obs_info['SEFDs_freq'] = [0.0]
+obs_info['SEFDs_freq_up'] = [0.0]
+
+for fn in split_fns:
+    print('Searching 100: %s' % fn)
+    find_seti_event = FinDoppler(fn, max_drift = 100.0, snr = 25.0, out_dir = output_dir+'search_100/', obs_info=obs_info)
+    find_seti_event.search()
+    print('Finishing searching %s' % fn)
+
+print('Finished searching everything with turbo_seti!')
+
+######
+
+print('Starting dataframe search')
+# Sort files with and without Doppler drifted hits
+no_hits = []
+hits_no_doppler = []
+hits_with_doppler = []
+
+df = pd.DataFrame()
+for fn in split_fns:
+    head, tail = os.path.split(fn)
+    
+    csv = head + 'search_100/' + tail[:-4] + '.dat'
+    
+    # print('On %s' % csv)
+    names = ['Top_Hit_#', 'Drift_Rate', 'SNR', 'Uncorrected_Frequency', 'Corrected_Frequency', 'Index', 'freq_start', 'freq_end', 'SEFD', 'SEFD_freq', 'Coarse_Channel_Number', 'Full_number_of_hits']
+    dataframe = pd.read_csv(csv, delim_whitespace=True, comment='#', names=names)
+    
+    if len(dataframe) == 0:
+        no_hits.append(i)
+    elif all(dataframe['Drift_Rate'] == 0.0):
+        hits_no_doppler.append(i)
+    else:
+        hits_with_doppler.append(i)
+print('# no hits: %s' % len(no_hits))
+print('# hits no doppler: %s' % len(hits_no_doppler))
+print('# hits with doppler: %s' % len(hits_with_doppler))
+
+with open(output_dir+'search_100/no_hits.txt', 'w') as no_hits_file:
+    for index in no_hits:
+        no_hits_file.write('%s\n' % index)
+with open(output_dir+'search_100/hits_no_doppler.txt', 'w') as hits_no_doppler_file:
+    for index in hits_no_doppler:
+        hits_no_doppler_file.write('%s\n' % index)
+with open(output_dir+'search_100/hits_with_doppler.txt', 'w') as hits_with_doppler_file:
+    for index in hits_with_doppler:
+        hits_with_doppler_file.write('%s\n' % index)
+        
+print('Finished writing out to file')
+        
+
+
+
+######
+    
+for fn in split_fns:
+    print('Searching 8000: %s' % fn)
+    find_seti_event = FinDoppler(fn, max_drift = 100.0, snr = 25.0, out_dir = output_dir+'search_8000/', obs_info=obs_info)
+    find_seti_event.search()
+    print('Finishing searching %s' % fn)
+
+print('Finished searching everything with turbo_seti!')
+
+######
+
+print('Starting dataframe search')
+# Sort files with and without Doppler drifted hits
+no_hits = []
+hits_no_doppler = []
+hits_with_doppler = []
+
+df = pd.DataFrame()
+for fn in split_fns:
+    head, tail = os.path.split(fn)
+    
+    csv = head + 'search_8000/' + tail[:-4] + '.dat'
+    
+    # print('On %s' % csv)
+    names = ['Top_Hit_#', 'Drift_Rate', 'SNR', 'Uncorrected_Frequency', 'Corrected_Frequency', 'Index', 'freq_start', 'freq_end', 'SEFD', 'SEFD_freq', 'Coarse_Channel_Number', 'Full_number_of_hits']
+    dataframe = pd.read_csv(csv, delim_whitespace=True, comment='#', names=names)
+    
+    if len(dataframe) == 0:
+        no_hits.append(i)
+    elif all(dataframe['Drift_Rate'] == 0.0):
+        hits_no_doppler.append(i)
+    else:
+        hits_with_doppler.append(i)
+print('# no hits: %s' % len(no_hits))
+print('# hits no doppler: %s' % len(hits_no_doppler))
+print('# hits with doppler: %s' % len(hits_with_doppler))
+
+with open(output_dir+'search_8000/no_hits.txt', 'w') as no_hits_file:
+    for index in no_hits:
+        no_hits_file.write('%s\n' % index)
+with open(output_dir+'search_8000/hits_no_doppler.txt', 'w') as hits_no_doppler_file:
+    for index in hits_no_doppler:
+        hits_no_doppler_file.write('%s\n' % index)
+with open(output_dir+'search_8000/hits_with_doppler.txt', 'w') as hits_with_doppler_file:
+    for index in hits_with_doppler:
+        hits_with_doppler_file.write('%s\n' % index)
+        
+print('Finished writing out to file')

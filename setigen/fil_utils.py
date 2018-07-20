cd5a480ce3e987cea054c4743d32e50c18a9332a
Bryan Brzycki <bbrzycki@berkeley.edu>
2018-07-19 17:59:26 -0700

    Generalize fil_utils to include both filename and Waterfall inputs

--- setigen/fil_utils.py
+++ setigen/fil_utils.py
@@ -4,27 +4,41 @@ import errno
 import numpy as np
 from blimpy import read_header, Waterfall, Filterbank
 
-def maxfreq(input_fn):
+
+def maxfreq(input):
     """Return central frequency of the highest-frequency bin in a .fil file.
 
     """
-    return read_header(input_fn)[b'fch1']
+    if type(input) == str:
+        fch1 = read_header(input)[b'fch1']
+    elif type(input) == Waterfall:
+        fch1 = input.header[b'fch1']
+    else:
+        sys.exit('Invalid input file!')
+    return fch1
 
-def minfreq(input_fn):
+def minfreq(input):
     """Return central frequency of the lowest-frequency bin in a .fil file.
 
     """
-    fch1 = read_header(input_fn)[b'fch1']
-    nchans = read_header(input_fn)[b'nchans']
-    ch_bandwidth = read_header(input_fn)[b'foff']
+    if type(input) == str:
+        fch1 = read_header(input)[b'fch1']
+        nchans = read_header(input)[b'nchans']
+        ch_bandwidth = read_header(input)[b'foff']
+    elif type(input) == Waterfall:
+        fch1 = input.header[b'fch1']
+        nchans = input.header[b'nchans']
+        ch_bandwidth = input.header[b'foff']
+    else:
+        sys.exit('Invalid input file!')
     return fch1 + nchans * ch_bandwidth
 
-def get_data(input_fn):
+def get_data(input):
     """Gets time-frequency data from filterbank file as a 2d NumPy array.
 
     Parameters
     ----------
-    input_fn : str
+    input : str
         Name of filterbank file
 
     Returns
@@ -32,15 +46,20 @@ def get_data(input_fn):
     data : ndarray
         Time-frequency data
     """
-    fil = Waterfall(input_fn)
+    if type(input) == str:
+        fil = Waterfall(input)
+    elif type(input) == Waterfall:
+        fil = input
+    else:
+        sys.exit('Invalid input file!')
     return np.squeeze(fil.data)
 
-def get_fs(input_fn):
+def get_fs(input):
     """Gets frequency values from filterbank file.
 
     Parameters
     ----------
-    input_fn : str
+    input : str
         Name of filterbank file
 
     Returns
@@ -48,17 +67,24 @@ def get_fs(input_fn):
     fs : ndarray
         Frequency values
     """
-    fch1 = read_header(input_fn)[b'fch1']
-    df = read_header(input_fn)[b'foff']
-    fchans = read_header(input_fn)[b'nchans']
+    if type(input) == str:
+        fch1 = read_header(input)[b'fch1']
+        df = read_header(input)[b'foff']
+        fchans = read_header(input)[b'nchans']
+    elif type(input) == Waterfall:
+        fch1 = input.header[b'fch1']
+        df = input.header[b'foff']
+        fchans = input.header[b'nchans']
+    else:
+        sys.exit('Invalid input file!')
     return np.arange(fch1, fch1 + fchans * df, df)
 
-def get_ts(input_fn):
+def get_ts(input):
     """Gets time values from filterbank file.
 
     Parameters
     ----------
-    input_fn : str
+    input : str
         Name of filterbank file
 
     Returns
@@ -66,6 +92,25 @@ def get_ts(input_fn):
     ts : ndarray
         Time values
     """
-    tsamp = read_header(input_fn)[b'tsamp']
-    tchans = get_data(input_fn).shape[0]
-    return np.arange(0, tchans*tsamp, tsamp)
+    if type(input) == str:
+        tsamp = read_header(input)[b'tsamp']
+    elif type(input) == Waterfall:
+        tsamp = input.header[b'tsamp']
+    else:
+        sys.exit('Invalid input file!')
+
+    try:
+        tchans = get_data(input).shape[0]
+    except Exception as e:
+        if type(input) == str:
+            fch1 = read_header(input)[b'fch1']
+            df = read_header(input)[b'foff']
+        else:
+            fch1 = input.header[b'fch1']
+            df = input.header[b'foff']
+        fil0 = Waterfall(input, f_start = fch1, f_stop = fch1 + df)
+        try:
+            tchans = get_data(fil0).shape[0]
+        except Exception as e:
+            sys.exit('No data in filterbank file!')
+    return np.arange(0, tchans * tsamp, tsamp)

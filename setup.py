cbfa8ab19978cf88b2d7a9b6a3abf9a148755f52
Bryan Brzycki <bryan@bryanbrzycki.com>
2020-03-16 21:03:13 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='setigen',
-    version='1.2.0',
+    version='1.2.1',
     author='Bryan Brzycki',
     author_email='bbrzycki@berkeley.edu',
     description='SETI radio signal generator',

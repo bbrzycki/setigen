1cfb321986cdbba03557d7de2f66d58f3c3d044c
Bryan Brzycki <bbrzycki@berkeley.edu>
2020-10-26 17:47:25 -0700

    Update version number in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2018-2020, Bryan Brzycki'
 author = u'Bryan Brzycki'
 
 # The short X.Y version
-version = u'1.2'
+version = u'1.3'
 # The full version, including alpha/beta/rc tags
-release = u'1.2.0'
+release = u'1.3.2'
 
 
 # -- General configuration ---------------------------------------------------

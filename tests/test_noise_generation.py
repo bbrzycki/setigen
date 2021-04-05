12df4117ce0e9f6d0f98e76f42f548a56254b38b
Bryan Brzycki <bbrzycki@berkeley.edu>
2021-04-05 16:12:06 -0700

    Update vanilla stg tests

--- tests/test_noise_generation.py
+++ tests/test_noise_generation.py
@@ -14,7 +14,7 @@ def frame_setup_hres():
     frame = stg.Frame(fchans=1024*u.pixel,
                       tchans=32*u.pixel,
                       df=2.7939677238464355*u.Hz,
-                      dt=18.25361108*u.s,
+                      dt=18.253611008*u.s,
                       fch1=6095.214842353016*u.MHz)
     return frame
 
@@ -33,13 +33,13 @@ def test_hres_chi2(frame_setup_hres):
     frame = copy.deepcopy(frame_setup_hres)
     frame.add_noise(10, noise_type='chi2')
     
-    assert abs(np.mean(frame.get_data()) / 10 - 1) < 0.01
-    assert abs(np.std(frame.get_data()) / (10 * (2 / (4 * 51))**0.5) - 1) < 0.01
+    assert abs(np.mean(frame.get_data()) / 10 - 1) < 0.02
+    assert abs(np.std(frame.get_data()) / (10 * (2 / (4 * 51))**0.5) - 1) < 0.02
     
     
 def test_very_hres_chi2(frame_setup_very_hres):
     frame = copy.deepcopy(frame_setup_very_hres)
     frame.add_noise(10, noise_type='chi2')
     
-    assert abs(np.mean(frame.get_data()) / 10 - 1) < 0.01
-    assert abs(np.std(frame.get_data()) / (10 * (2 / (4 * 2))**0.5) - 1) < 0.01
+    assert abs(np.mean(frame.get_data()) / 10 - 1) < 0.02
+    assert abs(np.std(frame.get_data()) / (10 * (2 / (4 * 2))**0.5) - 1) < 0.02

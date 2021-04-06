87953bd5592f76971d6f8da746df17dd3b6d550d
Bryan Brzycki <bbrzycki@berkeley.edu>
2021-04-05 17:31:24 -0700

    Update version in anticipation of pull

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='setigen',
-    version='1.4.3',
+    version='2.0.0',
     author='Bryan Brzycki',
     author_email='bbrzycki@berkeley.edu',
     description='SETI radio signal generator',

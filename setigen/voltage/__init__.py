3ec475f34025c5366e3d056695cf81ff564283d1
Bryan Brzycki <bbrzycki@berkeley.edu>
2021-04-25 14:53:30 -0700

    Fix init

--- setigen/voltage/__init__.py
+++ setigen/voltage/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 from .data_stream import *
-from .antennas import *
+from .antenna import *
 from .polyphase_filterbank import *
 from .quantization import *
 from .waterfall import *

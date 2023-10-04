b8a0512edf1d81af69f32bbe2a4cf8f2488972cb
Bryan Brzycki <bbrzycki@berkeley.edu>
2023-10-04 11:50:13 -0700

    Explicit imports for __init__

--- setigen/__init__.py
+++ setigen/__init__.py
@@ -1,17 +1,31 @@
 from __future__ import absolute_import, division, print_function
 
-from setigen.funcs import *
+from setigen.funcs import (
+    constant_path, squared_path, sine_path, simple_rfi_path,
+    constant_t_profile, sine_t_profile, periodic_gaussian_t_profile,
+    box_f_profile, gaussian_f_profile, multiple_gaussian_f_profile,
+    lorentzian_f_profile, voigt_f_profile, sinc2_f_profile,
+    bp_profile
+)
 from setigen import voltage
 
-from setigen.distributions import *
-from setigen.waterfall_utils import *
-from setigen.frame import *
-from setigen.sample_from_obs import *
-from setigen.split_utils import *
-from setigen.stats import *
-from setigen.unit_utils import *
-from setigen.frame_utils import *
-from setigen.normalize import *
-from setigen.dedrift import *
+from setigen.distributions import fwhm, gaussian, truncated_gaussian, chi2
+from setigen.waterfall_utils import (
+    max_freq, min_freq, get_data, get_fs, get_ts
+)
+from setigen.sample_from_obs import (
+    sample_gaussian_params, get_parameter_distributions, get_mean_distribution
+)
+from setigen.split_utils import (
+    split_waterfall_generator, split_fil, split_array
+)
+from setigen.stats import (
+    exclude_and_flatten, get_mean, get_std, get_min, compute_frame_stats
+)
+from setigen.unit_utils import cast_value, get_value
+from setigen.frame_utils import db, array, render, plot, integrate, get_slice
+from setigen.normalize import sigma_clip_norm, sliding_norm, max_norm
+from setigen.dedrift import dedrift
 
+from setigen.frame import Frame, params_from_backend
 from setigen.cadence import Cadence, OrderedCadence
--- setigen/funcs/__init__.py
+++ setigen/funcs/__init__.py
@@ -1,6 +1,11 @@
 from __future__ import absolute_import, division, print_function
 
-from .paths import *
-from .t_profiles import *
-from .f_profiles import *
-from .bp_profiles import *
+from .paths import constant_path, squared_path, sine_path, simple_rfi_path
+from .t_profiles import (
+    constant_t_profile, sine_t_profile, periodic_gaussian_t_profile
+)
+from .f_profiles import (
+    box_f_profile, gaussian_f_profile, multiple_gaussian_f_profile,
+    lorentzian_f_profile, voigt_f_profile, sinc2_f_profile
+)
+from .bp_profiles import bp_profile
